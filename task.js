61525e41ad2592b5ea82559777cd26c548c0901b
nodeMD <mdynieski@pm.me>
2022-01-04 10:49:33 +0100

    extended tasks descriptions

--- task.js
+++ task.js
@@ -2,6 +2,16 @@
 /*
 Task 1
 Import object user1 and write a function that will check if the phone number for every user is a digit
+Result:
+- if the phone number is a digit then log true
+- if the phone number is not a digit then log which user's phone number is not a digit
+something like: "Phone number of User with ID 2 is not a digit"
+
 Task 2
 Import objects user1 and user2 (this one is a js file, object is assigned to an exported const) and compare them
+Result:
+- if the users1 first email address is the same as users2 first email address (users1.users[1].emailAddress === users2.users[1].emailAddress) then do not log anything
+- if the users1 first email address is not the same as users2 first email address then log that information
+something like: "email address of user with ID 2 differs between user1 and user2 objects. User1: ${user1 email address}; user2: ${user2 email address}"
+compare email addresses, first names, last names and phone numbers
 */
