dd77bc4bdd84c3e4dfe804f22b9d0d1e38964963
nodeMD <mdynieski@pm.me>
2022-01-04 10:20:27 +0100

    added task

---
+++ task.js
@@ -0,0 +1,7 @@
+// ToDo
+/*
+Task 1
+Import object user1 and write a function that will check if the phone number for every user is a digit
+Task 2
+Import objects user1 and user2 (this one is a js file, object is assigned to an exported const) and compare them
+*/
