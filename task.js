66ee26e5491b9695b26b3782d90e56764436d871
Mrova96 <69799582+Mrova96@users.noreply.github.com>
2022-01-12 01:00:19 +0100

    Update task.js
    Task done :) 
    
    Few remarks:
    Task 1): It was simple and rather easy.
    Task 2): I believe having the same code few times to check different stuff is redundant and maybe another loop should be used or maybe switch:cases instead to make it more optimized. Yet I struggled a little bit with it so I send over something that works but it is not ideal per se. 
    
    Thanks for the challange! I had to learn quickly and it was fun :)

--- task.js
+++ task.js
@@ -1,16 +1,97 @@
-// ToDo
 /*
-Task 1
+Task 1 DONE last update: 11.01.2022 23:00
 Import array with users (from user1.json file) and write a function that will check if the phone number for every single user contains only digits
 Result:
 - if the phone number contains only digits then log true in the console
 - if the phone number does not contain only digits then log which user's phone number is not correct e.g. "Phone number of user with ID 2 is not correct"
 
-Task 2
+Task 2 IN-PROGRESS last update:
 Import array with users (from user1.json file) and users (from user2.js file) and compare them
 Result:
 - if the first email address of users1 is the same as the first email address of users2 (users1.users[1].emailAddress === users2.users[1].emailAddress) then do not log anything
 - if the first email address of users1 is not the same as the first email address of users2 then log in the console e.g. "Email addresses of user with ID 1 are not the same. First email is: racks.jacson@test.tt; but second email is: racks.jecson@test.tt".
-
 Compare email addresses, first names, last names and phone numbers.
 */
+
+
+//wykorzystanie NodeJS'owego file systemu
+const fs=require('fs');
+//Sparsowanie danych z zewnetrznego pliku JSON do obiektu JS dla ZADANIA 2
+var users1 = JSON.parse(fs.readFileSync('array.json', 'utf8'));
+
+//Import danych z pliku JS dla ZADANIA 1
+var users2 = require('./users2.js');
+
+
+
+
+//ZADANIE 1
+
+console.log("TASK 1: \n \n");
+
+
+function check_phone_numerics(importedArray_1)
+{
+        for (var FAValue of importedArray_1) {
+
+        if (isNaN(FAValue.phoneNumber)) {     //Jesli wartosc phonenumber z importowanej tablicy nie jest liczba
+
+            console.log("Phone number of user ID:", FAValue.userId, "is incorrect!");  //Wyswietl: string, wartosc userId z importowanego Arraya ,string
+
+        } else console.log(!isNaN(FAValue.phoneNumber)); //W innym wypadku: wyswietl wartosc logiczna problemu: czy wartosc phonenumber z importowanego arraya nie jest liczba?
+    }
+
+}
+
+check_phone_numerics(users1.users);  //Wywolanie funkcji uzywajacej zaimportowanej tablicy
+//check_phone_numerics(secondArray); //Test wykonany na drugiej tablicy - przechodzi :)
+
+//ZADANIE 2
+console.log("TASK 2: \n \n");
+
+
+
+//Email Check
+function compare_data(importedArray_1, importedArray_2) {
+
+
+for (var i = 0; i < importedArray_1.length; i++){
+            if (importedArray_1[i].emailAddress === importedArray_2[i].emailAddress) {
+
+            }
+            else console.log("MISMATCH FOUND!: Email addresses of user with ID", importedArray_1[i].userId ,"are not the same. Email in first array is:", importedArray_1[i].emailAddress, "but in the second array is::", importedArray_2[i].emailAddress)
+
+
+        }
+
+    for (var i = 0; i < importedArray_1.length; i++){
+        if (importedArray_1[i].firstName === importedArray_2[i].firstName) {
+
+        }
+        else console.log("MISMATCH FOUND!: First names of user with ID", importedArray_1[i].userId ,"are not the same. First name in first array  is:", importedArray_1[i].firstName, "but in the second array is:", importedArray_2[i].firstName)
+
+    }
+
+    for (var i = 0; i < importedArray_1.length; i++){
+        if (importedArray_1[i].lastName === importedArray_2[i].lastName) {
+
+        }
+        else console.log("MISMATCH FOUND!: Last names of user with ID", importedArray_1[i].userId ,"are not the same. Last name in first array  is:", importedArray_1[i].lastName, "but in the second array is:", importedArray_2[i].lastName)
+
+    }
+
+    for (var i = 0; i < importedArray_1.length; i++){
+        if (importedArray_1[i].phoneNumber === importedArray_2[i].phoneNumber) {
+
+        }
+        else console.log("MISMATCH FOUND!: Phone numbers of user with ID", importedArray_1[i].userId ,"are not the same. Phone number in first array  is:", importedArray_1[i].phoneNumber, "but in the second array is:", importedArray_2[i].phoneNumber)
+
+    }
+
+}
+compare_data(users1.users, users2.users);
+
+
+
+
+
