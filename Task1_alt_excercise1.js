18cfd2a8b907c385fccdcf9ee6b3e6819f545fd3
Mrova96 <69799582+Mrova96@users.noreply.github.com>
2022-01-12 08:59:49 +0100

    Update Task1_alt_excercise1.js

--- Task1_alt_excercise1.js
+++ Task1_alt_excercise1.js
@@ -4,11 +4,11 @@ function check_phone_numerics(importedArray_1)
         for (var FAValue of importedArray_1) {
             var regex = new RegExp( /^[0-9]+$/);
 
-        if(importedArray_1.phoneNumber.match(regex)) {     //Jesli wartosc phonenumber z importowanej tablicy nie zawiera tylko liczb
+        if(!importedArray_1.phoneNumber.match(regex)) {     //Jesli wartosc phonenumber z importowanej tablicy nie zawiera tylko liczb
 
             console.log("Phone number of user ID:", FAValue.userId, "is incorrect!");  //Wyswietl: string, wartosc userId z importowanego Arraya ,string
 
-        } else console.log(!isNaN(FAValue.phoneNumber)); //W innym wypadku: wyswietl wartosc logiczna rozwiazania problemu: czy wartosc phonenumber z importowanego arraya nie jest liczba?
+        } else console.log(!importedArray_1.phoneNumber.match(regex))); //W innym wypadku: wyswietl wartosc logiczna rozwiazania problemu
     }
 
 }
